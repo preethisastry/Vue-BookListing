7c33c0df27720603e5cd8931aae9ab2b5d7b92b2
Preethi Kondur <preethisastry@outlook.com>
2020-02-27 16:00:48 -0500

    Completed module

--- src/components/BookList.vue
+++ src/components/BookList.vue
@@ -1,6 +1,12 @@
 <template>
     <div>
         <h1> {{ title }} </h1>
+        <ul>
+            <li v-for = "book in books" :key = "book.title">
+                <span> {{ book.title }}: {{ book.author }}  </span>
+            </li>
+            
+        </ul>
     </div>
 </template>
 
@@ -10,13 +16,31 @@
         data() {
             return {
                 title:'All Books',
+            
+            books: [
+                {title:'Self-Reliance', author: 'Ralph Waldo Emerson'},
+                {title:'American Gods', author: 'Neil Gaiman'},
+                {title:'Amusing Ourselves to Death', author: 'Neil Postman'}
+
+            ],
             };
          },
+
     };
 </script>
 
 <style>
     h1, h2{
         font-weight: normal;
+    }
+
+    ul{
+        list-style-type: none;
+        padding: 0;
+    }
+
+    li{
+        display: block;
+        margin: 0 10px;
     }
 </style>
